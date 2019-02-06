a6f4abb985252081f6525091195be728b8e0277c
sahedsawon <smsawoncse@gmail.com>
2019-02-06 11:03:38 +0600

    API key removed from repo

--- videos/src/API/YouTube.js
+++ videos/src/API/YouTube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyAhbleoX93SdQwk5gM5TfxW0MEuCPPcD5I';
+const API_KEY = 'YOUR_YOUTUBE_API_KEY';
 
 export default axios.create({
     baseURL : 'https://www.googleapis.com/youtube/v3',
