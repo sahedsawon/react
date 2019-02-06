a38643107310cd4e6767f78861a4ea4530125df0
sahedsawon <smsawoncse@gmail.com>
2019-02-06 22:56:53 +0600

    redux and react-redux libraby added

---
+++ rx/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import App from './components/App';
+
+ReactDOM.render(
+    <App />
+, document.getElementById('root'));
